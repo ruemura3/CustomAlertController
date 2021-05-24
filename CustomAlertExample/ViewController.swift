7ca8c2db6b4f326d1cc876e769555a4bde406245
pcmbryo <pcmbr.magic@gmail.com>
2021-05-24 19:31:24 +0900

    Initial Commit

---
+++ CustomAlertExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomAlertExample
+//
+//  Created by Ryo UEMURA on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
